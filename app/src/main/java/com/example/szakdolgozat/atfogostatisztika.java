4ee392a2e63319c6ab6cd6308d6e1e91d395894d
levi19982 <leventevass19982@gmail.com>
2022-04-01 14:24:23 +0200

    Initial Commit

--- app/src/main/java/com/example/szakdolgozat/atfogostatisztika.java
+++ app/src/main/java/com/example/szakdolgozat/atfogostatisztika.java
@@ -6,22 +6,42 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.core.Tag;
+import com.google.firebase.database.snapshot.Index;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class atfogostatisztika extends AppCompatActivity {
 
+    ArrayList<Integer> ertekek2;
+    ArrayList<String> ertekek;
+
+    BarChart barChart;
+
+    BarData barData;
+
+    BarDataSet barDataSet;
 
 
     @Override
@@ -29,50 +49,55 @@ public class atfogostatisztika extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atfogostatisztika);
 
+        barChart = findViewById(R.id.idBarChart);
+
+
         Intent intent = getIntent();
         String kapottsportag = intent.getStringExtra("kapottsportag");
 
         DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://szakdolgozat-9d551-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("Sportok").child(kapottsportag);
-        DatabaseReference databaseReference2 = FirebaseDatabase.getInstance("https://szakdolgozat-9d551-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("Sportok").child(kapottsportag);
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                ArrayList<String> ertekek = new ArrayList<>();
-
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
-                    String adat = dataSnapshot.getKey();
-                    ertekek.add(adat);
+                valami(snapshot);
+                valami2(snapshot);
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                Log.v(TAG, ertekek.get(0));
+                Log.v(TAG, ertekek2.get(0).toString());
+                ArrayList<BarEntry> barEntries = new ArrayList<>();
+                for (int i = 0; i < ertekek.size(); i++){
+                    for (int a = 0; a < ertekek2.size(); a++){
+                        String datum = ertekek.get(i);
+                        int datumszam = Integer.parseInt(datum);
+                        int jelentkezok = ertekek2.get(a);
+                        //String jelentkezokszama = String.valueOf(jelentkezok);
+                        barEntries.add(new BarEntry(datumszam, jelentkezok));
+                    }
                 }
-                Log.d("item id ",ertekek.get(0));
-                Log.d("item id ",ertekek.get(2));
-                Log.d("item id ",ertekek.get(3));
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
+    }
 
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                ArrayList<Integer> ertekek2 = new ArrayList<>();
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
-                    int a = (int) (dataSnapshot.getChildrenCount()-1);
-                    ertekek2.add(a);
-                }
-                Log.d("item id ",ertekek2.get(0).toString());
-                Log.d("item id ",ertekek2.get(1).toString());
-                Log.d("item id ",ertekek2.get(3).toString());
-            }
+    public ArrayList valami2(DataSnapshot snapshot) {
+        ertekek2 = new ArrayList<>();
+        for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+            int a = (int) (dataSnapshot.getChildrenCount()-1);
+            ertekek2.add(a);
+        }
+        return ertekek2;
+    }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
+    public ArrayList valami(DataSnapshot snapshot) {
+        ertekek = new ArrayList<>();
+        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+            String adat = dataSnapshot.getKey();
+            ertekek.add(adat);
+        }
+        return ertekek;
     }
 }
