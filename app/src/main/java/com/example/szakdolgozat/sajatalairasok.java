25a65433b0bc6ea93e2afb8787c463635639c465
levi19982 <leventevass19982@gmail.com>
2022-04-10 16:01:26 +0200

    Valami

--- app/src/main/java/com/example/szakdolgozat/sajatalairasok.java
+++ app/src/main/java/com/example/szakdolgozat/sajatalairasok.java
@@ -1,5 +1,7 @@
 package com.example.szakdolgozat;
 
+import static com.google.android.gms.common.internal.safeparcel.SafeParcelable.NULL;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,17 +26,19 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class sajatalairasok extends AppCompatActivity {
 
     FirebaseDatabase adatbazis;
     DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://szakdolgozat-9d551-default-rtdb.europe-west1.firebasedatabase.app").getReference();
     DatabaseReference databaseReference1, databaseReference2, databaseReference3;
-    TextView alairasok;
+    TextView alairasok,ijaszat, konditerem, tanc;
     EditText nev;
     Button alairasokszam;
-    int elofordulasok = 0;
     ArrayList<String> nevek = new ArrayList<>();
+    ArrayList<String> nevek2 = new ArrayList<>();
+    ArrayList<String> nevek3 = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,31 +46,94 @@ public class sajatalairasok extends AppCompatActivity {
         setContentView(R.layout.activity_sajatalairasok);
 
         alairasok = findViewById(R.id.osszesalairas);
+        konditerem = findViewById(R.id.textView4);
+        tanc = findViewById(R.id.textView6);
         nev = findViewById(R.id.kapottnev);
         alairasokszam = findViewById(R.id.alairasokszama);
+        ijaszat = findViewById(R.id.ijaszatalairas);
         databaseReference1 = databaseReference.child("Sportok").child("Íjászat");
+        databaseReference2 = databaseReference.child("Sportok").child("Konditerem");
+        databaseReference3 = databaseReference.child("Sportok").child("Tánc");
 
 
         alairasokszam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                databaseReference1.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        nevek.clear();
-                        for (DataSnapshot dataSnapshot : snapshot.getChildren()){
-                                if (dataSnapshot.child(nev.getText().toString()).exists()){
-                                nevek.add(dataSnapshot.child(nev.getText().toString()).child("nev").getValue().toString());
-                                elofordulasok++;
+                if (nev.getText().toString().isEmpty()) {
+                    Toast.makeText(sajatalairasok.this, "Kérjük írja be nevét!", Toast.LENGTH_SHORT).show();
+                } else {
+                    databaseReference1.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            int elofordulasok = 0;
+                            ijaszat.setText("");
+                            nevek.clear();
+                            for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                if (dataSnapshot.child(nev.getText().toString()).exists()) {
+                                    nevek.add(dataSnapshot.child(nev.getText().toString()).child("nev").getValue().toString());
+                                    elofordulasok++;
+                                }
                             }
+                            ijaszat.setText(Integer.toString(elofordulasok));
                         }
-                    }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
 
-                    }
-                });
+                        }
+                    });
+                    databaseReference2.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            int elofordulasok2 = 0;
+                            konditerem.setText("");
+                            nevek2.clear();
+                            for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                if (dataSnapshot.child(nev.getText().toString()).exists()) {
+                                    nevek2.add(dataSnapshot.child(nev.getText().toString()).child("nev").getValue().toString());
+                                    elofordulasok2++;
+                                }
+                            }
+                            konditerem.setText(Integer.toString(elofordulasok2));
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                    databaseReference3.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            int elofordulasok3 = 0;
+                            tanc.setText("");
+                            nevek3.clear();
+                            for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                if (dataSnapshot.child(nev.getText().toString()).exists()) {
+                                    nevek3.add(dataSnapshot.child(nev.getText().toString()).child("nev").getValue().toString());
+                                    elofordulasok3++;
+                                }
+                            }
+                            tanc.setText(Integer.toString(elofordulasok3));
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                    int osszesalairas = 0;
+                    if (!ijaszat.getText().toString().equals("")) {
+                            osszesalairas += Integer.parseInt(ijaszat.getText().toString());
+                        }
+                        if (!konditerem.getText().toString().equals("")) {
+                            osszesalairas += Integer.parseInt(konditerem.getText().toString());
+                        }
+                        if (!tanc.getText().toString().equals("")) {
+                            osszesalairas += Integer.parseInt(tanc.getText().toString());
+                        }
+                    alairasok.setText(String.valueOf(osszesalairas));
+                }
             }
         });
     }
