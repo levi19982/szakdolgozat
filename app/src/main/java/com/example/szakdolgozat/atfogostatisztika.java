9d34bcd54ea3455013b04e8603a95f3f9ec3a7dc
levi19982 <leventevass19982@gmail.com>
2022-09-21 22:10:18 +0200

    Valami

--- app/src/main/java/com/example/szakdolgozat/atfogostatisztika.java
+++ app/src/main/java/com/example/szakdolgozat/atfogostatisztika.java
@@ -1,65 +1,40 @@
 package com.example.szakdolgozat;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 
-import com.github.mikephil.charting.charts.BarChart;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Line;
+import com.anychart.data.Mapping;
+import com.anychart.data.Set;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.MarkerType;
+import com.anychart.enums.TooltipPositionMode;
+import com.anychart.graphics.vector.Stroke;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
-import com.github.mikephil.charting.formatter.ValueFormatter;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
-import com.google.firebase.Timestamp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.collection.LLRBNode;
-import com.google.firebase.database.core.Tag;
-import com.google.firebase.database.snapshot.Index;
 
-import org.apache.commons.collections4.trie.AbstractBitwiseTrie;
-import org.checkerframework.checker.units.qual.C;
-
-import java.sql.Time;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
+import java.util.List;
 
 public class atfogostatisztika extends AppCompatActivity {
 
@@ -72,70 +47,40 @@ public class atfogostatisztika extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atfogostatisztika);
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        anyChartView.setProgressBar(findViewById(R.id.progress_bar));
 
-        lineChart = findViewById(R.id.linechart);
-
-
-
-
-
-        felugroablak();
-
-
-
-    }
-
-    public void barchart(){
-
-    }
-
-    public void linechart(){
-        ArrayList<Integer> szamok = new ArrayList<>();
-        ArrayList<String> datumok = new ArrayList<>();
-        String[] datumo1k = {"Január", "Február"};
         Intent intent = getIntent();
         String kapottsport = intent.getStringExtra("kapottsportag");
-        ArrayList<Entry> vegso = new ArrayList<>();
+
+        Cartesian cartesian = AnyChart.line();
+
+        cartesian.animation(true);
+
+        cartesian.padding(10d, 20d, 5d, 20d);
+
+        cartesian.crosshair().enabled(true);
+        cartesian.crosshair()
+                .yLabel(true)
+                // TODO ystroke
+                .yStroke((Stroke) null, null, null, (String) null, (String) null);
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+
+        cartesian.title(kapottsport + " részvételei.");
+
+        List<DataEntry> seriesData = new ArrayList<>();
+
+
         DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://szakdolgozat-9d551-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("Sportok").child(kapottsport);
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                for (DataSnapshot snapshot1 : snapshot.getChildren()){
                     String adat = snapshot1.getKey();
-                    datumok.add(adat);
-                    String[] ertekek = adat.split("-");
-                    int nap = Integer.parseInt(ertekek[2]);
-                    int honap = Integer.parseInt(ertekek[1]);
-                    int ev = Integer.parseInt(ertekek[0]);
-                    int egyesitett = Integer.parseInt(adat.replace("-",""));
-                    szamok.add(egyesitett);
-                    int szamokseged = (int) (snapshot1.getChildrenCount() - 1);
-                        vegso.add(new Entry(egyesitett, szamokseged));
-                        //Toast.makeText(atfogostatisztika.this, String.valueOf(egyesitett), Toast.LENGTH_SHORT).show();
+                    long szamokseged = (snapshot1.getChildrenCount() - 1);
+                    seriesData.add(new CustomDataEntry(adat, szamokseged));
                 }
-                LineDataSet lineDataSet = new LineDataSet(vegso, "Próba");
-                ArrayList<ILineDataSet> iLineDataSets = new ArrayList<>();
-                iLineDataSets.add(lineDataSet);
-
-                XAxis xAxis = lineChart.getXAxis();
-                xAxis.setValueFormatter(new IndexAxisValueFormatter(datumok));
-
-                xAxis.setGranularity(2f);
-                YAxis yAxisleft = lineChart.getAxisLeft();
-                yAxisleft.setGranularity(1f);
-                YAxis yAxisright = lineChart.getAxisRight();
-                yAxisright.setGranularity(1f);
-
-                xAxis.setTextColor(Color.RED);
-                yAxisleft.setTextColor(Color.RED);
-
-
-                LineData lineData = new LineData(iLineDataSets);
-                lineChart.setData(lineData);
-                lineChart.invalidate();
-
-
-
             }
 
             @Override
@@ -143,46 +88,44 @@ public class atfogostatisztika extends AppCompatActivity {
 
             }
         });
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Set set = Set.instantiate();
+                set.data(seriesData);
+                Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
+
+                Line series1 = cartesian.line(series1Mapping);
+                series1.name(kapottsport);
+                series1.hovered().markers().enabled(true);
+                series1.hovered().markers()
+                        .type(MarkerType.CIRCLE)
+                        .size(4d);
+                series1.tooltip()
+                        .position("right")
+                        .anchor(Anchor.LEFT_CENTER)
+                        .offsetX(5d)
+                        .offsetY(5d);
+
+
+                cartesian.legend().enabled(true);
+                cartesian.legend().fontSize(13d);
+                cartesian.legend().padding(0d, 0d, 10d, 0d);
+
+                anyChartView.setChart(cartesian);
+            }
+        }, 5000);
+
+
     }
 
-    private void felugroablak() {
-        builder = new AlertDialog.Builder(this);
-        View view = getLayoutInflater().inflate(R.layout.valasztas, null);
-        spinner = view.findViewById(R.id.melyikchart);
+    private class CustomDataEntry extends ValueDataEntry {
 
-        builder.setView(view);
-        alertDialog = builder.create();
-        alertDialog.show();
-        String[] sportok = getResources().getStringArray(R.array.chartok);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, sportok);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                switch (i) {
-                    case 0:
-                        break;
-                    case 1:
-                        linechart();
-                        break;
-                    case 2:
-                        barchart();
-                        break;
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-    }
-    private class ctengelyertekek extends ValueFormatter implements IAxisValueFormatter{
-
-        @Override
-        public String getFormattedValue(float value, AxisBase axis) {
-            return "Időpont" + value;
+        CustomDataEntry(String x, Number value) {
+            super(x, value);
         }
+
     }
+
 }
