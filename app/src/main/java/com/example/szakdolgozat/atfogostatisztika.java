1fe46c8ce1e4f677b165571493d02bde54718ca1
levi19982 <leventevass19982@gmail.com>
2022-09-22 11:28:47 +0200

    Valami

--- app/src/main/java/com/example/szakdolgozat/atfogostatisztika.java
+++ app/src/main/java/com/example/szakdolgozat/atfogostatisztika.java
@@ -22,11 +22,15 @@ import android.widget.Toast;
 
 
 import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Bar;
+import com.anychart.core.cartesian.series.JumpLine;
 import com.anychart.core.cartesian.series.Line;
 import com.anychart.data.Mapping;
 import com.anychart.data.Set;
 import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
 import com.anychart.enums.MarkerType;
+import com.anychart.enums.TooltipDisplayMode;
 import com.anychart.enums.TooltipPositionMode;
 import com.anychart.graphics.vector.Stroke;
 import com.github.mikephil.charting.charts.LineChart;
@@ -78,7 +82,8 @@ public class atfogostatisztika extends AppCompatActivity {
                         alertDialog.dismiss();
                         break;
                     case 2:
-                        Toast.makeText(atfogostatisztika.this, "Erre kattintottál!", Toast.LENGTH_SHORT).show();
+                        verticalchart();
+                        alertDialog.dismiss();
                         break;
                 }
             }
@@ -88,6 +93,76 @@ public class atfogostatisztika extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void verticalchart(){
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        anyChartView.setProgressBar(findViewById(R.id.progress_bar));
+
+        Intent intent = getIntent();
+        String kapottsport = intent.getStringExtra("kapottsportag");
+
+        Cartesian cartesian = AnyChart.vertical();
+
+        cartesian.animation(true).title(kapottsport + " részvételei");
+
+        List<DataEntry> adatok = new ArrayList<>();
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://szakdolgozat-9d551-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("Sportok").child(kapottsport);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                    String adat = snapshot1.getKey();
+                    long szamokseged = (snapshot1.getChildrenCount() - 1);
+                    adatok.add(new CustomDataEntry(adat, szamokseged));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+        Set set = Set.instantiate();
+        set.data(adatok);
+
+        Mapping mapping = set.mapAs("{ x: 'x', value: 'value' }");
+        Mapping jumpLineData = set.mapAs("{ x: 'x', value: 'jumpLine' }");
+
+        Bar bar = cartesian.bar(mapping);
+        bar.labels().format("Résztvevők száma");
+
+        JumpLine jumpLine = cartesian.jumpLine(jumpLineData);
+        jumpLine.stroke("2 #60727B");
+        jumpLine.labels().enabled(false);
+
+        cartesian.yScale().minimum(0d);
+
+        cartesian.labels(true);
+
+        cartesian.tooltip()
+                .displayMode(TooltipDisplayMode.UNION)
+                .positionMode(TooltipPositionMode.POINT)
+                .unionFormat(
+                        "function() {\n" +
+                                "      return 'Résztvevők száma: ' + this.points[1].value +\n" +
+                                "        '\\n'" +
+                                "    }");
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+
+        cartesian.xAxis(true);
+        cartesian.yAxis(true);
+        cartesian.yAxis(0).labels().format("{%value}{type:number, decimalsCount:0}");
+
+        anyChartView.setChart(cartesian);
+            }
+        }, 5000);
+
     }
 
     public void linechart(){
@@ -111,7 +186,7 @@ public class atfogostatisztika extends AppCompatActivity {
 
         cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
 
-        cartesian.title(kapottsport + " részvételei.");
+        cartesian.title(kapottsport + " részvételei");
 
         List<DataEntry> seriesData = new ArrayList<>();
 
