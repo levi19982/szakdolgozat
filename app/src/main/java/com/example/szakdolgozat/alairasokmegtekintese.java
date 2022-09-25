b26af86d07e0b109e8cf2d797722ae29a7e465bc
levi19982 <leventevass19982@gmail.com>
2022-09-25 18:11:57 +0200

    Valami

--- app/src/main/java/com/example/szakdolgozat/alairasokmegtekintese.java
+++ app/src/main/java/com/example/szakdolgozat/alairasokmegtekintese.java
@@ -1,17 +1,28 @@
 package com.example.szakdolgozat;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class alairasokmegtekintese extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class alairasokmegtekintese extends ListActivity {
+
+    ArrayList<String> elsokep = new ArrayList<>();
+    ArrayList<String> masodikkep = new ArrayList<>();
+    ArrayList<String> jelentkezettekneptun = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alairasokmegtekintese);
 
-        
+        ListView listView = (ListView) findViewById(R.id.jelentkezettekalairas);
+
+        listaadapter adapter = new listaadapter(this, elsokep, masodikkep, jelentkezettekneptun);
+        listView.setAdapter(adapter);
 
     }
 }
